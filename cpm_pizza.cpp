60e397398c83e538c14754a1584f03ae2a933352
Marcel Bonnici <marcelbonnici2020@u.northwestern.edu>
2021-09-29 00:22:34 -0400

    Nodes input code compressed

--- cpm_pizza.cpp
+++ cpm_pizza.cpp
@@ -1,15 +1,10 @@
-#include <cstdio>
-#include <memory>
-#include <stdexcept>
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <memory>
 #include <queue>
-#include <string>
 #include <stack>
-
-#define DBG 1   // set DBG 1 for debugging code and 0 for normal run
-#define PLOT_GRAPH 1 // set 1 for plotting graph if system meets necessary requirements
+#include <tuple>
+#define DBG 1   // 1 for debugging, 0 for normal run
 
 using namespace std;
 
@@ -23,142 +18,42 @@ std::string exec(const char* cmd) {
     char buffer[128];
     std::string result = "";
     std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) throw std::runtime_error("popen() failed!");
     while (!feof(pipe.get())) {
         if (fgets(buffer, 128, pipe.get()) != NULL)
-            result += buffer;
+            result=result+buffer;
     }
     return result;
 }
 
-// returns vector of n numbers for input
-std::vector<int> ReadNumbers()
-{
-    std::vector<int> numbers ;
+void topologicalSortUtil(int v, vector<bool> &visited,  stack<int> &Stack, vector< vector<int> > &adj);
 
-    do
-    {
-        int input ;
-        if ( std::cin >> input ) //if user input a successor that int (aka a valid input)
-            numbers.push_back(input) ; //put at back of vector
-    }while ( std::cin && std::cin.peek() != '\n' ); //repeat while input's valid and next character we wanna push isn't 'Enter' key
+struct cpmStart {
+    vector<string> name1;
+		vector<int> duration1;
+		int n_tasks;
+		int tweak;
+};
 
-    return numbers ;
-}
-
-// utility for topological sorting of activity graph
-void topologicalSortUtil(int v, vector<bool> &visited,  stack<int> &Stack, vector< vector<int> > &adj)
-{
-    visited[v] = true;
-
-    vector<int>::iterator i; //Points at memory address of STL container, often with numbers.
-		//Spares time & complexity.
-    for (i = adj[v].begin(); i != adj[v].end(); ++i) //!= instead of <? Why? Because adj #s don't ascend, necessarily? i++ is same as ++i in this code's high level case
-        if (!visited[*i]) //if contents are still false | *i=CONTENTS!
-            topologicalSortUtil(*i, visited, Stack, adj); // *i is contents at element 1, and up
-
-    Stack.push(v);
-}
+typedef struct cpmStart Struct;
+Struct cpm_input(ofstream &f);
+Struct foodnodes(Struct super, string foodnumber, int tweak, vector<string> name1, vector<int> duration1, int n_tasks);
 
 int main() {
-	int MAX=10;
-	vector<string> name1;
-	vector<int> duration1;
-	vector<string> foodcombo;
 	bool cheese_passed=false;
-	int i=0, n_tasks=0, top=0, j=0, items = 0, tweak = 0;
+	int top=0;
 	ofstream f; //means by which we can open external files
-	f.open("plot_graph.plt");
 
-	cout<<"############## Critical Path management ################\n\n";
-	cout<<"How many items will you be ordering today? ";
-	cin>>items;
-
-	//nodes[n_tasks+1].name = "Finish";
-	//nodes[n_tasks+1].duration = 0;
-	name1.push_back("Courier");
-	duration1.push_back(6);
-	n_tasks++;
-
-	name1.push_back("Stov_Of");
-	duration1.push_back(5);
-	n_tasks++;
-
-	// input of all the tasks
-	for(i = 1 ; i <= items; i++) {
-		cout<<"\n\nDish #"<<i<<" - Enter (0) for Pizza, (1) for Calzone, (2) for Grilled Cheese : ";
-		std::string foodnumber;
-		std::cin >> foodnumber;
-
-		if (foodnumber == "0"){
-			//foodcombo.push_back("0");
-			tweak++;
-			name1.push_back("RemoveD");
-			duration1.push_back(2);
-			name1.push_back("Cheese");
-			duration1.push_back(4);
-			name1.push_back("Apply_D");
-			duration1.push_back(1);
-			n_tasks=n_tasks+3;
-		}
-		else if (foodnumber=="1"){
-			//foodcombo.push_back("1");
-			name1.push_back("RemoveZ");
-			duration1.push_back(2);
-			name1.push_back("Apply_Z");
-			duration1.push_back(3);
-			n_tasks=n_tasks+2;
-		}
-		else if (foodnumber=="2"){
-			if (tweak==1){
-				name1.pop_back();
-				duration1.pop_back();
-				name1.pop_back();
-				duration1.pop_back();
-				name1.pop_back();
-				duration1.pop_back();
-			}
-			tweak=tweak+2;
-			name1.push_back("RemoveB");
-			duration1.push_back(2);
-			name1.push_back("Apply_B");
-			duration1.push_back(8);
-			name1.push_back("Cheese");
-			duration1.push_back(4);
-			name1.push_back("Apply_B");
-			duration1.push_back(8);
-			n_tasks=n_tasks+4;
-			if (tweak==3){
-				name1.push_back("RemoveD");
-				duration1.push_back(2);
-				name1.push_back("Cheese");
-				duration1.push_back(4);
-				name1.push_back("Apply_D");
-				duration1.push_back(1);
-			}
-		}
-	}
-
-	name1.push_back("Stov_On");
-	duration1.push_back(7);
-	n_tasks++;
-
-
-	if(PLOT_GRAPH) {
-		if (tweak==3){
-			f<<n_tasks-1<<endl;
-		}
-		else{
-			f<<n_tasks<<endl;
-		}
-		f<<"Start 0"<<endl;
-	}
+	Struct superduper;
+	superduper = cpm_input(f);
+	vector<string> name1 = superduper.name1;
+	vector<int> duration1 = superduper.duration1;
+	int n_tasks = superduper.n_tasks;
 
 	struct activity nodes[n_tasks+3];
 	nodes[0].name = "Start";
 	nodes[0].duration = 0;
 
-	if (n_tasks==MAX){
+	if (n_tasks==10){
 		nodes[n_tasks].name = "Deliver";
 		nodes[n_tasks].duration = 0;
 		}
@@ -168,7 +63,7 @@ int main() {
 	}
 
 	for (int i=1; i<=n_tasks; i++){
-			if (n_tasks==MAX){
+			if (n_tasks==10){
 				if ((i==3 || i==4)&&name1.back()=="Cheese"){
 					cheese_passed=true;
 				}
@@ -187,37 +82,22 @@ int main() {
 			}
 		name1.pop_back();
 		duration1.pop_back();
-/*
-		if(PLOT_GRAPH) {
-			f<<nodes[i].name<<" "<<nodes[i].duration<<endl;
-		}*/
+
 	}
 
 	for (int i=1; i<=n_tasks; i++){
-		if(PLOT_GRAPH) {//NEW
-			f<<nodes[i].name<<" "<<nodes[i].duration<<endl;
-		} //NEW
+		f<<nodes[i].name<<" "<<nodes[i].duration<<endl;
 	}
 
-	if(PLOT_GRAPH && n_tasks!=MAX) {
+	if(n_tasks!=10) {
 		f<<"Deliver 0"<<endl;
 	}
-	if(n_tasks==MAX){
+	else{
 		n_tasks--;
 	}
-		/*
-		nodes[i].name=foodnumber;
-		//cin>>nodes[i].name;
-
-		cout<<"Enter duration for "<<i<<" : ";
-		cin>>nodes[i].duration;
-		if(PLOT_GRAPH) {
-			f<<nodes[i].name<<" "<<nodes[i].duration<<endl;
-		}
-	}*/
 
 	cout<<"\n\n\t\tTasks entered :\n";
-	for(i = 0 ; i <= n_tasks+1; i++) { //upper limit could be i < n_tasks+2, no?
+	for(int i = 0 ; i <= n_tasks+1; i++) { //upper limit could be i < n_tasks+2, no?
 		cout<<"\t\t"<<i<<". "<<nodes[i].name<<" "<<nodes[i].duration<<endl;
 	}
 
@@ -226,7 +106,7 @@ int main() {
 
 
 	// initialization of both lists with empty vectors
-	for(i = 0 ; i <= n_tasks; i++) {
+	for(int i = 0 ; i <= n_tasks; i++) {
 		vector<int> temp;
 		adj.push_back(temp);
 		pred.push_back(temp);
@@ -235,7 +115,7 @@ int main() {
 	// initialization of successor list based on user input
 	// NOTE : User need to input all the tasks with no predecessors as the successor of "Start"
 	cout<<"\n\nNOTE : User need to input all the tasks with no predecessors as the successor of \"Start\"";
-	for(i = 0 ; i <= n_tasks; i++) {
+	for(int i = 0 ; i <= n_tasks; i++) {
 		vector<int> ReadNumbers;
 		cout<<"\n\nEnter successors for task "<<nodes[i].name<<" : ";
 		if (i==1){
@@ -254,7 +134,7 @@ int main() {
 							}
 						}
 					}
-					else{//else{
+					else{
 						ReadNumbers.push_back(j);
 					}
 				}
@@ -346,13 +226,11 @@ int main() {
 				ReadNumbers.push_back(i+1);//22: Type 2, then 1. Conditional looks more like the opposite (12), as do all the conditonals under this "else"
 			}
 		}
-		vector<int> temp = ReadNumbers;//();
+		vector<int> temp = ReadNumbers;
 		if(temp.size()==0){
 			adj[i].push_back(n_tasks);
 			pred[n_tasks].push_back(i);
-			if(PLOT_GRAPH) {
-				f<<i<<" "<<n_tasks<<endl;
-			}
+			f<<i<<" "<<n_tasks<<endl;
 		}
 
 		if(temp.size()!=0) {
@@ -366,10 +244,8 @@ int main() {
 				}
 				pred[temp[j]].push_back(i);
 			}
-			if(PLOT_GRAPH) {
-				for(int j=0;j<temp.size();j++){
-					f<<i<<" "<<temp[j]<<endl;
-				}
+			for(int j=0;j<temp.size();j++){
+				f<<i<<" "<<temp[j]<<endl;
 			}
 		}
 	}
@@ -377,29 +253,26 @@ int main() {
 	if(DBG) {
 		//debugging
 		cout<<"\nSuccessor matrix :\n";
-		for(i = 0 ; i < n_tasks+2; i++) {
+		for(int i = 0 ; i < n_tasks+2; i++) {
 			cout<<i<<" : ";
-			for(j = 0 ; j < adj[i].size(); j++) {
+			for(int j = 0 ; j < adj[i].size(); j++) {
 				cout<<adj[i][j]<<"->";
 			}
 			cout<<endl;
 		}
 
 		cout<<"Predecessor matrix :\n";
-		for(i = 0 ; i < n_tasks+2; i++) {
+		for(int i = 0 ; i < n_tasks+2; i++) {
 			cout<<i<<" : ";
-			for(j = 0 ; j < pred[i].size(); j++) {
+			for(int j = 0 ; j < pred[i].size(); j++) {
 				cout<<pred[i][j]<<"->";
 			}
 			cout<<endl;
 		}
 	}
 
-
-
 	// calculating earliest start and finish times for each task
 	// topological sort of task is required here
-
 	stack<int> Stack;
 	vector<bool> visit(n_tasks+2, false);
 	topologicalSortUtil(0,visit, Stack, adj);
@@ -410,9 +283,8 @@ int main() {
 
 	while(!Stack.empty()) {
 		top = Stack.top();
-		//cout<<"top "<<top<<endl;
 		int max_f = -1;
-		for(i = 0; i < pred[top].size(); i++) {
+		for(int i = 0; i < pred[top].size(); i++) {
 			if(max_f < nodes[pred[top][i]].ef) {
 				max_f = nodes[pred[top][i]].ef;
 			}
@@ -425,7 +297,7 @@ int main() {
 
 	if(DBG) {
 		cout<<"Es and Ef : \n";
-		for(i = 0 ; i < n_tasks+2; i++) {
+		for(int i = 0 ; i < n_tasks+2; i++) {
 			cout<<i<<" "<<nodes[i].name<<" "<<nodes[i].es<<" "<<nodes[i].ef<<endl;
 		}
 	}
@@ -444,7 +316,7 @@ int main() {
 		top = Stack2.top();
 		//cout<<"top "<<top<<endl;
 		int min_s = 99999;
-		for(i = 0; i < adj[top].size(); i++) {
+		for(int i = 0; i < adj[top].size(); i++) {
 			if(min_s > nodes[adj[top][i]].ls) {
 				min_s = nodes[adj[top][i]].ls;
 			}
@@ -456,17 +328,14 @@ int main() {
 
 	if(DBG) {
 		cout<<"Ls and Lf : \n";
-		for(i = 0 ; i < n_tasks+2; i++) {
+		for(int i = 0 ; i < n_tasks+2; i++) {
 			cout<<i<<" "<<nodes[i].name<<" "<<nodes[i].ls<<" "<<nodes[i].lf<<endl;
 		}
 	}
 
-
-	// display of results
-
 	cout<<"RESULTS : \n\n";
 	cout<<"\t#\tTask\tDur.\tEs\tEf\tLs\tLf\tST\n\n";
-	for(i = 0 ; i < n_tasks+2 ; i++) {
+	for(int i = 0 ; i < n_tasks+2 ; i++) {
 		nodes[i].st = nodes[i].ls - nodes[i].es;
 		cout<<"\t"<<i<<"\t"<<nodes[i].name<<"\t"<<nodes[i].duration<<"\t"<<nodes[i].es<<"\t"<<nodes[i].ef<<"\t"<<nodes[i].ls<<"\t"<<nodes[i].lf<<"\t"<<nodes[i].st<<"\n\n";
 	}
@@ -475,7 +344,6 @@ int main() {
 	// finding the critical path
 	// simple BFS can be done to find critical path
 	queue<int> q3;
-	//vector<int> visited3(n_tasks+2,0); //redundant
 	vector<int> critical_path(n_tasks+2,0);
 	q3.push(0);
 	critical_path[0] = 1;
@@ -485,15 +353,13 @@ int main() {
 		if(nodes[top].es == nodes[top].ls) {
 			critical_path[top] = 1;
 		}
-		for(i = 0 ; i < adj[top].size(); i++) {
-			//if(visited3[adj[top][i]] == 0){
+		for(int i = 0 ; i < adj[top].size(); i++) {
 				q3.push(adj[top][i]);
-			//}
 		}
 	}
 
 	cout<<"Critical Path : ";
-	for(i = 0 ; i < critical_path.size(); i++) {
+	for(int i = 0 ; i < critical_path.size(); i++) {
 		if(critical_path[i]==1){
 		 cout<<nodes[i].name<<"->";
 		 f<<i<<" ";
@@ -503,7 +369,102 @@ int main() {
 	f<<endl;
 	f.close();
 	exec("python3 plot_graph2.py < plot_graph.plt");
-
 	return 0;
+}
 
+void topologicalSortUtil(int v, vector<bool> &visited,  stack<int> &Stack, vector< vector<int> > &adj)
+{
+    visited[v] = true;
+
+    vector<int>::iterator i; //Points at memory address of STL container, often with numbers.
+		//Spares time & complexity.
+    for (i = adj[v].begin(); i != adj[v].end(); ++i) //!= instead of <? Why? Because adj #s don't ascend, necessarily? i++ is same as ++i in this code's high level case
+        if (!visited[*i]) //if contents are still false | *i=CONTENTS!
+            topologicalSortUtil(*i, visited, Stack, adj); // *i is contents at element 1, and up
+    Stack.push(v);
+}
+
+Struct cpm_input(ofstream &f){
+	vector<string> name1;
+	vector<int> duration1;
+	int n_tasks=0, items = 0, tweak = 0;
+	Struct super, fantastico;
+	f.open("plot_graph.plt");
+	cout<<"############## Critical Path management ################\n\n";
+	while (items<1){
+		cout<<"How many items will you be ordering today? ";
+		cin>>items;
+	}
+
+	name1.push_back("Courier");
+	duration1.push_back(6);
+	name1.push_back("Stov_Of");
+	duration1.push_back(5);
+	n_tasks=2;
+
+	// input of all the tasks
+	for(int i = 1 ; i <= items; i++) {
+		cout<<"\n\nDish #"<<i<<" - Enter (0) for Pizza, (1) for Calzone, (2) for Grilled Cheese : ";
+		std::string foodnumber;
+		std::cin >> foodnumber;
+
+		fantastico = foodnodes(fantastico, foodnumber, tweak, name1, duration1, n_tasks); //need to introduce int tweak
+		name1 = fantastico.name1; duration1 = fantastico.duration1;
+		n_tasks = fantastico.n_tasks;
+		tweak = fantastico.tweak;
+	}
+
+	name1.push_back("Stov_On");
+	duration1.push_back(7);
+	n_tasks++;
+
+	if (tweak==3){
+		f<<n_tasks-1<<endl;
+	}
+	else{
+		f<<n_tasks<<endl;
+	}
+	f<<"Start 0"<<endl;
+
+	super.name1 = name1;
+	super.duration1 = duration1;
+	super.n_tasks = n_tasks;
+	return super;
+}
+
+Struct foodnodes(Struct fantastico, string foodnumber, int tweak, vector<string> name1, vector<int> duration1, int n_tasks){
+	if (foodnumber == "0"){
+		tweak++;
+		name1.push_back("RemoveD"); duration1.push_back(2);
+		name1.push_back("Cheese"); duration1.push_back(4);
+		name1.push_back("Apply_D"); duration1.push_back(1);
+		n_tasks=n_tasks+3;
+	}
+	else if (foodnumber=="1"){
+		name1.push_back("RemoveZ"); duration1.push_back(2);
+		name1.push_back("Apply_Z"); duration1.push_back(3);
+		n_tasks=n_tasks+2;
+	}
+	else if (foodnumber=="2"){
+		if (tweak==1){
+			name1.pop_back(); duration1.pop_back();
+			name1.pop_back(); duration1.pop_back();
+			name1.pop_back(); duration1.pop_back();
+		}
+		tweak=tweak+2;
+		name1.push_back("RemoveB"); duration1.push_back(2);
+		name1.push_back("Apply_B"); duration1.push_back(8);
+		name1.push_back("Cheese"); duration1.push_back(4);
+		name1.push_back("Apply_B"); duration1.push_back(8);
+		n_tasks=n_tasks+4;
+		if (tweak==3){
+			name1.push_back("RemoveD"); duration1.push_back(2);
+			name1.push_back("Cheese"); duration1.push_back(4);
+			name1.push_back("Apply_D"); duration1.push_back(1);
+		}
+	}
+	fantastico.name1 = name1; fantastico.duration1 = duration1;
+	fantastico.tweak = tweak;
+	fantastico.n_tasks = n_tasks;
+	return fantastico;
 }
