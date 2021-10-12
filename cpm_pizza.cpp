449f0f399975179e6e6dbba2c169ef480be9f020
Marcel Bonnici <marcelbonnici2020@u.northwestern.edu>
2021-10-12 00:36:30 -0400

    Everything before optional hardware done

--- cpm_pizza.cpp
+++ cpm_pizza.cpp
@@ -5,50 +5,8 @@
 #include <stack>
 #include <tuple>
 #define DBG 1   // 1 for debugging, 0 for normal run
-
 using namespace std;
-
-namespace CPMBegin
-{
-    class ObjectManager
-    {
-    public:
-			vector<string> name1;
-			vector<int> duration1;
-			int n_tasks;
-			int tweak;
-		  CPMBegin::ObjectManager cpm_input(ofstream &f);
-			CPMBegin::ObjectManager foodnodes(CPMBegin::ObjectManager super, string foodnumber, int tweak, vector<string> name1, vector<int> duration1, int n_tasks);
-			string name;
-			int duration;
-			int es, ef, ls, lf, st;
-			CPMBegin::ObjectManager* start_end_nodes(int n_tasks, CPMBegin::ObjectManager *nodes);
-			// This function vvv courtesy of https://github.com/suman95/Critical-path-management
-			void topologicalSortUtil(int v, vector<bool> &visited,  stack<int> &Stack, vector< vector<int> > &adj);
-			string exec(const char* cmd);
-			CPMBegin::ObjectManager* task_nodes(int n_tasks, vector<string> name1, vector<int> duration1, CPMBegin::ObjectManager *nodes);
-			int nodes_to_plot(ofstream &f, CPMBegin::ObjectManager *nodes, int n_tasks);
-			void tasks_print(CPMBegin::ObjectManager *nodes, int n_tasks);
-			vector< vector<int> > adj;
-			vector< vector<int> > pred;
-			CPMBegin::ObjectManager make_adj_pred(int n_tasks);
-			//vector<int> ReadNumbers;
-			CPMBegin::ObjectManager fill_adj_pred(vector<int> ReadNumbers, int n_tasks, int i, vector<vector<int>> adj, vector<vector<int>> pred, ofstream &f);
-			void debug_matrices(int n_tasks, vector<vector<int>> adj, vector< vector<int> > pred);
-      CPMBegin::ObjectManager make_edges(int n_tasks, CPMBegin::ObjectManager *nodes, vector<vector<int>> adj, vector<vector<int>> pred, ofstream &f);
-			void results_table(int n_tasks, CPMBegin::ObjectManager *nodes);
-			void critical_path1(int n_tasks, CPMBegin::ObjectManager *nodes, vector<vector<int>> adj, ofstream &f);
-			CPMBegin::ObjectManager *nodes;
-			CPMBegin::ObjectManager* esef(int n_tasks, vector<vector<int>> adj, CPMBegin::ObjectManager* nodes, vector<vector<int>> pred);
-			CPMBegin::ObjectManager lslf(int n_tasks, vector<vector<int>> adj, vector<vector<int>> pred, CPMBegin::ObjectManager *nodes);
-      vector<int> ReadNumbers;
-      vector<int> make_02_edges(int i, vector<int> ReadNumbers, int n_tasks);
-      vector<int> make_20_edges(int i, vector<int> ReadNumbers, int n_tasks);
-      vector<int> make_stov_on_edges (int i, int n_tasks, CPMBegin::ObjectManager* nodes, vector<int> ReadNumbers);
-      vector<int> easy_case(int i, vector<int> ReadNumbers, CPMBegin::ObjectManager *nodes);
-      vector<int> other_cases(int i, vector<int> ReadNumbers, CPMBegin::ObjectManager* nodes, int n_tasks);
-		};
-}
+#include "cpm_pizza.h"
 
 string CPMBegin::ObjectManager::exec(const char* cmd) {
     char buffer[128];
@@ -67,8 +25,8 @@ void CPMBegin::ObjectManager::topologicalSortUtil(int v, vector<bool> &visited, 
 		CPMBegin::ObjectManager tsu;
     vector<int>::iterator i; //Points at memory address of STL container, often with numbers.
 		//Spares time & complexity.
-    for (i = adj[v].begin(); i != adj[v].end(); ++i) //!= instead of <? Why? Because adj #s don't ascend, necessarily? i++ is same as ++i in this code's high level case
-        if (!visited[*i]) //if contents are still false | *i=CONTENTS!
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i]) //if contents are still false | *i=CONTENTS
             tsu.topologicalSortUtil(*i, visited, Stack, adj); // *i is contents at element 1, and up
     Stack.push(v);
 }
